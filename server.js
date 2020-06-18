cf0fbbc9b50c38c45d561048ee2d08f88f7721d6
mysteryinfosolutions <47215871+mysteryinfosolutions@users.noreply.github.com>
2020-06-18 11:58:57 +0530

    full start

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+const port=3000;
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(port,()=>{
+    console.log("listenng to port ${port}");
+});
